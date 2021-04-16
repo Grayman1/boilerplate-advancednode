c0295436f21375af1113d0083e631bd0844785b7
Grayman1 <>
2021-04-16 18:18:24 +0000

    Challenge #1 Install pug, view engine, views. Alltests PASSED!!

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
+const pug = require('pug');
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+});
+
+
+app.set('view engine', 'pug')
+app.set('views', './views/pug')
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,10 +22,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render('index');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
-});
+
+
+
+
+
+
+
